dcb6343194a4bcba9a4170d05e7c7d76ab409a0c
buiduycuong97 <duycuongbui97@gmail.com>
2023-12-20 11:34:59 +0700

    fix port

--- src/utils/axiosFetchApi.js
+++ src/utils/axiosFetchApi.js
@@ -32,7 +32,7 @@ export async function refreshAccessToken() {
     console.log(refreshToken);
     const response = await axios.post(
       // "http://52.74.248.97:8080/auth/refresh-token",
-      "http://localhost:8000/auth/refresh-token",
+      "http://52.74.248.97:8000/auth/refresh-token",
       {},
       {
         headers: {
